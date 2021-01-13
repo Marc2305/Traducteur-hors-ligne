34e075b269a83201b99dc1e1c415623b2a3a3397
Marc <marc.candessoussenswoc@gmail.com>
2021-01-13 23:12:32 +0100

    Nouveau commit

--- search.js
+++ search.js
@@ -3,17 +3,31 @@ const AnnexeFRListA = ["ajoute","allemagne","aller","américain","amour","anglet
     "archer","arme","armes","autorise","avoir"];
 const LengthAnnexeFRListA = AnnexeFRListA.length;
 
-const AnnexeFRListB = [];
+const AnnexeFRListB = ["banque","bateau","bière","bilingue","billet","blanc","blanche",
+    "blanches","blouson","boit","bouteille","bureau","bus",];
 const LengthAnnexeFRListB = AnnexeFRListB.length;
+
+const AnnexeFRListC = ["café","calendrier","canard","capitaine","chante","chaussure",
+"chaussures","chemise","cheval","chiens","chinois","cible","cibles","cinéma","citron",
+"cloche","costume","coupe","court","couteau","cuisine",];
+const LengthAnnexeFRListC = AnnexeFRListC.length;
+
+const AnnexeFRListD = ["demain","désolé","déteste","dimanche","disponible","dix-sept",
+    "document","documents","dois","dormes","dort","douze","drapeau",];
+const LengthAnnexeFRListD = AnnexeFRListD.length;
 
 let searchName = "";
 
 function Search (searchName) {
     let result = 0;
+    let iIndex = 0;
 
-    for (let i = 0; i >LengthAnnexeFRListA ; i++){
+    for (iIndex = 0; i >LengthAnnexeFRListA ; i++){
         if(searchName === AnnexeFRListA[i]){
             result = 1;
+            return(
+                AnnexeFRListA
+            )
         }
         else {
             result = 0;
@@ -21,9 +35,27 @@ function Search (searchName) {
     }
 
     if (result === 0) {
-        for (let i = 0; i >LengthAnnexeFRListB ; i++){
+        for (iIndex = 0; i >LengthAnnexeFRListB ; i++){
             if(searchName === AnnexeFRListB[i]){
                 result = 1;
+                return(
+                    AnnexeFRListA
+                )
+            }
+            else {
+                result = 0;
+            }
+        }
+    }
+
+
+    if (result === 0) {
+        for (iIndex = 0; i >LengthAnnexeFRListC ; i++){
+            if(searchName === AnnexeFRListC[i]){
+                result = 1;
+                return(
+                    AnnexeFRListA
+                )
             }
             else {
                 result = 0;
