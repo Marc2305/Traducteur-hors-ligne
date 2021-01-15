f36a3ae5371356d43092506d515c5719dbc6e744
Marc <marc.candessoussenswoc@gmail.com>
2021-01-15 23:22:44 +0100

    Nouveau commit

--- search.js
+++ search.js
@@ -1,31 +1,3 @@
-const AnnexeFRListA = ["ajoute","allemagne","aller","américain","amour","angleterre","animal",
-    "animaux","août", "appeler","appelle","apprendre","après","après-midi","arbre","arc",
-    "archer","arme","armes","autorise","avoir"];
-const LengthAnnexeFRListA = AnnexeFRListA.length;
-
-
-const AnnexeFRListB = ["banque","bateau","bière","bilingue","billet","blanc","blanche",
-    "blanches","blouson","boit","bouteille","bureau","bus"];
-const LengthAnnexeFRListB = AnnexeFRListB.length;
-
-
-const AnnexeFRListC = ["café","calendrier","canard","capitaine","chante","chaussure",
-"chaussures","chemise","cheval","chiens","chinois","cible","cibles","cinéma","citron",
-"cloche","costume","coupe","court","couteau","cuisine"];
-const LengthAnnexeFRListC = AnnexeFRListC.length;
-
-
-const AnnexeFRListD = ["demain","désolé","déteste","dimanche","disponible","dix-sept",
-    "document","documents","dois","dormes","dort","douze","drapeau"];
-const LengthAnnexeFRListD = AnnexeFRListD.length;
-
-
-const AnnexeFRListE = ["écrire","écris","écrivain","efficace","église","églises",
-    "éléphant", "élèves", "elle", "enfant", "entre", "essayer", "étude", "européen",
-    "exemple", "explication"];
-const LengthAnnexeFRListE = AnnexeFRListE.length;
-
-
 let searchName = "";
 
 function Search (searchName) {
